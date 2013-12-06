187988504dad9428df339484bf14bd7d8182d117
Toomas Romer <toomas@zeroturnaround.com>
2013-12-06 20:44:49 +0200

    Do nothing hostname verifier

--- src/main/java/org/zeroturnaround/jenkins/reporter/JenkinsIgnoreSslClient.java
+++ src/main/java/org/zeroturnaround/jenkins/reporter/JenkinsIgnoreSslClient.java
@@ -1,11 +1,17 @@
 package org.zeroturnaround.jenkins.reporter;
 
+import java.io.IOException;
 import java.security.cert.CertificateException;
 import java.security.cert.X509Certificate;
+
+import javax.net.ssl.SSLException;
+import javax.net.ssl.SSLSession;
+import javax.net.ssl.SSLSocket;
 
 import org.apache.http.conn.scheme.Scheme;
 import org.apache.http.conn.ssl.SSLSocketFactory;
 import org.apache.http.conn.ssl.TrustStrategy;
+import org.apache.http.conn.ssl.X509HostnameVerifier;
 
 public class JenkinsIgnoreSslClient extends JenkinsHttpClient {
   public JenkinsIgnoreSslClient(String username, String authToken, Integer sslPort) {
@@ -19,7 +25,27 @@ public class JenkinsIgnoreSslClient extends JenkinsHttpClient {
           return true;
         }
 
-      });
+      },
+          new X509HostnameVerifier() {
+
+            @Override
+            public boolean verify(String arg0, SSLSession arg1) {
+              return true;
+            }
+
+            @Override
+            public void verify(String host, SSLSocket ssl) throws IOException {
+            }
+
+            @Override
+            public void verify(String host, X509Certificate cert) throws SSLException {
+            }
+
+            @Override
+            public void verify(String host, String[] cns, String[] subjectAlts) throws SSLException {
+            }
+
+          });
 
       httpClient.getConnectionManager().getSchemeRegistry().register(new Scheme("https", sslPort, sslsf));
     }
